757c100a66f061055e6fa106f7c973a1ec93ea99
GeraltWang <wgeralt@outlook.com>
2024-03-29 16:36:37 +0800

    refactor: ♻️ ts error

--- packages/unknown-ui/src/button/index.ts
+++ packages/unknown-ui/src/button/index.ts
@@ -2,7 +2,7 @@ import type { App } from 'vue'
 import Button from './button.vue'
 
 Button.install = (app: App) => {
-  app.component(Button.name, Button)
+  app.component(Button.name!, Button)
 }
 
 export default Button
