be3b50e33ff0e17ac7584fa2acc7015937392f00
GeraltWang <wgeralt@outlook.com>
2024-07-23 09:25:27 +0800

    ci: 👷 适应新的 eslint 插件

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,46 @@
-import mist from '@mistjs/eslint-config'
+import antfu from '@antfu/eslint-config'
+import { FlatCompat } from '@eslint/eslintrc'
 
-export default mist({
+const compat = new FlatCompat()
+
+export default antfu({
+  // Enable stylistic formatting rules
+  // stylistic: true,
+
+  // Or customize the stylistic rules
+  stylistic: {
+    indent: 2, // 4, or 'tab'
+    quotes: 'single', // or 'double'
+    jsx: true,
+  },
+
+  // TypeScript and Vue are auto-detected, you can also explicitly enable them:
+  typescript: true,
+  vue: true,
+
+  // Disable jsonc and yaml support
+  jsonc: false,
+  yaml: false,
+  // formatters: {
+  //   css: true,
+  //   html: true,
+  // },
+
+  // `.eslintignore` is no longer supported in Flat config, use `ignores` instead
+  ignores: ['node_modules', 'dist', '!.vscode', '!.github', '!.devcontainer'],
+}, {
   rules: {
     'no-console': 'off',
-    'no-debugger': 'off',
-    'no-tabs': 'off',
-    'style/no-tabs': 'off',
+    'unused-imports/no-unused-vars': 'off',
+    'regexp/no-unused-capturing-group': 'off',
+    'antfu/top-level-function': 'off',
+    'antfu/consistent-list-newline': 'off',
+    'jsdoc/require-returns-description': 'off',
   },
-})
+}, ...compat.config({
+  // extends: ['./.eslintrc-auto-import.json'],
+  // globals: {
+  //   __APP_INFO__: true,
+  //   process: true,
+  // },
+}))
